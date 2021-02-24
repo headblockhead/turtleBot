0051ed170d2bab8fc1ed2f06c939a5f9c80549d4
headblockhead <26520767+headblockhead@users.noreply.github.com>
2021-02-24 13:49:37 +0000

    first commit

---
+++ text_controller.py
@@ -0,0 +1,50 @@
+from turtle import *
+def turtle_controller(do,val):
+    do = do.upper()
+    if do == "F":
+        forward(val)
+    elif do == "B":
+        backward(val)
+    elif do == "R":
+        right (val)
+    elif do == "L":
+        left(val)
+    elif do == "U":
+        penup()
+    elif do == "D":
+        pendown()
+    elif do == "N":
+        reset()
+    else:
+        print ("Unreccognised comand")
+
+def string_artist(program):
+    cmd_list = program.split("-")
+    for command in cmd_list:
+        cmd_len = len(command)
+        if cmd_len == 0:
+            continue
+        cmd_type = command[0]
+        num = 0
+        if cmd_len > 1:
+            num_string = command[1:]
+            num = int (num_string)
+        print(command, ":", cmd_type, num)
+        turtle_controller(cmd_type, num)
+
+instructions = '''Enter a program for Headblockhead's turtle:
+e.g. F100-R45-U-F100-L45-D-F100-R90-B50
+N = New drawing
+U/D = Pen up/down
+F100 = Forward 100
+B50 = Backwards 50 
+R90 = Right turn 90 degrees
+L45 = Left turn 45 degrees'''
+
+screen = getscreen()
+while True:
+    t_program = screen.textinput("Drawing Machine", instructions)
+    print(t_program)
+    if t_program == None or t_program.upper() == "END":
+        break
+    string_artist(t_program)
